da80256e16f8f9570cfaf91fb4d08f54ceb79e01
Savin <savin.varshney@gmail.com>
2015-10-25 17:58:46 +0800

    added script for parsing cap to cvs and then storing to database

---
+++ parser.py
@@ -0,0 +1,39 @@
+#!/usr/local/bin/python3.4
+import csv
+
+f = open('captures/scan1.csv')
+csv_f = csv.reader(f)
+tmsi_list = []
+signal_list = []
+ci = ''
+mcc = ''
+mnc = ''
+lac = ''
+
+for row in csv_f:
+	if row[0]:
+		tmsi_list.append(row[0])
+		if row[1]:
+			if(row[1] and row[2]):
+				tmsi_list.append(row[1])
+				signal_list.append(int(row[2]))
+				signal_list.append(int(row[2]))
+			elif row[1]:
+				signal_list.append(int(row[1]))
+	if row[2] and not ci:
+		ci = row[2]
+	if row[3] and not mcc:
+		mcc = row[3]
+	if row[4] and not mnc:
+		mnc = row[4]
+	if row[5] and not lac:
+		lac = row[5]
+
+print (tmsi_list)
+print (len(tmsi_list))
+print (signal_list)
+print (len(signal_list))
+print (mcc)
+print (mnc)
+print (lac)
+print (ci)
