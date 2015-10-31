c965811fc40967fe922769b18c32c140a1a137e6
Savin <savin.varshney@gmail.com>
2015-10-31 14:46:08 +0800

    updated script and added bash generator script

---
+++ generator.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+args=("$@")
+echo ${args[0]}
+echo ${args[1]}
+
+tshark -r captures/${args[0]} -T fields -e gsm_a.tmsi -e gsmtap.signal_dbm -e gsm_a.bssmap.cell_ci -e e212.mcc -e e212.mnc -e gsm_a.lac -E separator=, > captures/${args[1]}
